521c6ce20356bbd42b7a72501f4813296486d841
VedantSharma18 <vedantsharma18@gmail.com>
2024-07-12 03:17:09 +0530

    added changs to incorporate functionalities

--- client/src/YummyYard.js
+++ client/src/YummyYard.js
@@ -9,11 +9,21 @@ import Axios from 'axios';
 import recipesData from './data/recipes';
 import AddRecipeForm from './Components/AddRecipeForm';
 
+
+//change 
+import withAuth from './Components/withAuth';
+//till here
+
 import Start_here from './pages/start_here';
 
 import LogSignup from './pages/logsignup';
 
 const YummyYard = () => {
+
+
+
+  const ProtectedAddRecipeForm = withAuth(AddRecipeForm);//change done
+
 
 const [token, setToken] = useState(null);
 
@@ -54,7 +64,11 @@ const [token, setToken] = useState(null);
           <Route exact path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/recipes" element={<RecipeList recipes={filteredRecipes} />} />
-          <Route path="/add-recipe" element={<AddRecipeForm onAddRecipe={handleAddRecipe} />} />
+
+          <Route path="/add-recipe" element={<ProtectedAddRecipeForm onAddRecipe={handleAddRecipe} />} />
+
+          {/* <Route path="/add-recipe" element={<AddRecipeForm onAddRecipe={handleAddRecipe} />} /> */}
+          
           <Route path="/start_here" element={<Start_here />} />
           <Route path="/log_signup" element={<LogSignup setToken={setToken} />} />
         </Routes>
