c6f30bfaad0b9dcfa48fe390dc19d70076b0da6b
VedantSharma18 <vedantsharma18@gmail.com>
2024-07-12 03:38:18 +0530

    Added Logout function

--- client/src/Components/navbar.jsx
+++ client/src/Components/navbar.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/style.css'
 import Logo from '../Assets/Logo1.png'
-
+import Logout from './Logout';
 
 const Navbar = () => {
+
+
+
+  //CHANGE MADE
+  const [isLoggedIn, setIsLoggedIn] = useState(false); 
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        setIsLoggedIn(true);
+    } else {
+        setIsLoggedIn(false);
+    }
+}, []);
+  //TILL HERE
+
+
+
+
+
   return (
     <header className="header">
       <nav className="navbar">
@@ -16,6 +35,15 @@ const Navbar = () => {
           <Link to="/about">About</Link>
           <Link to="/recipes">Recipes</Link>
           <Link to="/log_signup">Signup</Link>
+
+
+          {isLoggedIn && <Logout />}
+
+
+
+
+
+
         </div>
       </nav>
     </header>
