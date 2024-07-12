366719bfb83ea15a2cbe56e2957df83786b28396
VedantSharma18 <vedantsharma18@gmail.com>
2024-07-12 23:01:30 +0530

    comments

--- server/server.js
+++ server/server.js
@@ -15,22 +15,12 @@ connectToMongoDB();
 
 app.use(cors());
 
-// mongoose.connect('mongodb+srv://om_prakash103:W2wmCaagP77OlbyQ@recipesapp.h0btfh4.mongodb.net/', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// })
-// .then(() => {
-//     console.log('Connected to MongoDB');
-// })
-// .catch((err) => {
-//     console.error('MongoDB connection error:', err);
-// });
-  
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// db.once('open', () => {
-//     console.log('Connected to MongoDB');
-// });
+// app.use(cors({
+
+//     origin:['*'],credentials:true
+// }));
+
+
 
 app.use('/yummyYard/recipes', recipeRoutes);
 
@@ -44,3 +34,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`yummyYard server is listening on port ${PORT}`);
 });
+
+
+
+
+// module.exports = app;
