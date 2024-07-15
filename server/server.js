555afb321a0431ca9db4a059d9f73147191b8602
OmPrakash109 <om.ssb51@gmail.com>
2024-07-15 21:03:11 +0530

    Update: improved the search functionality

--- server/server.js
+++ server/server.js
@@ -13,38 +13,22 @@ app.use(express.json());
 
 connectToMongoDB();
 
-app.use(cors());
+app.use(cors({
 
-// app.use(cors({
+    origin:[
+        '*',
+         "https://yummy-yard-client.vercel.app"
+        ]
+    ,credentials:true
+}));
 
-//     origin:['*'],credentials:true
-// }));
-
-
-
-// mongoose.connect('mongodb+srv://om_prakash103:W2wmCaagP77OlbyQ@recipesapp.h0btfh4.mongodb.net/', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// })
-// .then(() => {
-//     console.log('Connected to MongoDB');
-// })
-// .catch((err) => {
-//     console.error('MongoDB connection error:', err);
-// });
-  
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// db.once('open', () => {
-//     console.log('Connected to MongoDB');
-// });
 
 app.use('/yummyYard/recipes', recipeRoutes);
 
 app.use('/yummyYard/recipes/log_signup/auth', authRoutes);
 
-app.get('/yummyYard', (req, res) => {
-    res.send("It is working..");
+app.get('/', async(req, res) => {
+    res.status(200).send("It is working..");
 } );
 
 const PORT = process.env.PORT || 5000;
@@ -55,4 +39,4 @@ app.listen(PORT, () => {
 
 
 
-//  module.exports = app;
+ module.exports = app;
